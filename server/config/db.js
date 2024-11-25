1a802a724be5ffe11470a7f3f405d04d6c7f8cbc
Shamim Al Mamun <shamim2601@gmail.com>
2024-11-25 23:05:35 +0600

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async()=>{
     try {
         mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect('mongodb+srv://shamim2601:6EkN3FJrQWhgoMX9@cluster0.o0etpjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        const conn = await mongoose.connect('mongodb+srv://shamim2601:6EkN3FJrQWhgoMX9@cluster0.o0etpjc.mongodb.net/');
         console.log(`Database connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
